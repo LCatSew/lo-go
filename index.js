81cc3009fc324e9dd03b7f6026fe5bfef6d2ca1f
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-05-26 15:21:32 -0500

    created the inquirer prompts index.js file

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ console.log(SHAPES);
 
 const shapes = new SHAPES();
 
-const promptUser = ()
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter up to 3 characters',
+            name: 'charInput',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a color keyword',
+            name: 'colorInput',
+        },
+        {
+            type: 'list',
+            message: 'What kind of shape would you like your license to be?',
+            choices: ['Circle', 'Triangle', 'Square'],
+            name: 'shapeSelect',
+        },
+    ])
+}
